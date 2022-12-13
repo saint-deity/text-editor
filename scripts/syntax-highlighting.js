203b6cd55ee1294e0b1a9aefcfce9b87a21ddb54
Saint Deity <deityecchi@gmail.com>
2022-12-13 22:52:07 +1000

    version is broken, but made commit to save progress revert to previous commit to get working version

--- scripts/syntax-highlighting.js
+++ scripts/syntax-highlighting.js
@@ -1,19 +1,44 @@
+// version is *not* viable for production use
 let keywords = [
   {
     "local": "hsl(258, 60%, 66%)",
+    "let": "hsl(258, 60%, 66%)",
   }
 ]
 
 // color the keywords
 function colorKeywords() {
   let text = document.getElementById('text');
+  // get index of users selection
+  let selectionIndex = text.selectionStart;
+  let letterbefore = true;
   // wrap the keywords in spans
   for (let i = 0; i < keywords.length; i++) {
-    let keyword = keywords[i];
-    let keywordName = Object.keys(keyword)[0];
-    let keywordColor = keyword[keywordName];
-    let keywordRegex = new RegExp(keywordName, 'g');
-    text.innerHTML = text.innerHTML.replace(keywordRegex, `<span style="color: ${keywordColor}">${keywordName}</span>`);
+
+    // get user selection position
+    // wrap every word in a span
+    for (let word in keywords[i]) {
+      let regex = new RegExp(word, 'g');
+      text.innerHTML = text.innerHTML.replace(regex, '<font color="' + keywords[i][word] + '">' + word + '</font>');
+    }
+
+    // send user select to last position
+    if (letterbefore) {
+      // create range
+      let range = document.createRange();
+      let sel = window.getSelection();
+
+      // transform selection index to
+      let selectionIndex = selectionIndex + 1;
+
+      // set range to last letter
+      range.setStart(text.childNodes[0], selectionIndex);
+      range.setEnd(text.childNodes[0], selectionIndex);
+
+      // set selection
+      sel.removeAllRanges();
+      sel.addRange(range);
+    }
   }
 }
 

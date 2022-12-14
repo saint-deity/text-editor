a17124d55b8f61df4b7a75e9510ddc76e3dbc0dd
Deity <59446525+saint-deity@users.noreply.github.com>
2022-12-14 23:14:09 +1000

    Partial fix to selection range
    Selection range breaks when keyword is highlighted.
    Fix should be done by EOW

--- scripts/syntax-highlighting.js
+++ scripts/syntax-highlighting.js
@@ -6,31 +6,78 @@ let keywords = [
   }
 ]
 
+var code = []
+
+// start of credit 
+// to Tim Down
+function getCaretPosition(editableDiv) {
+  var caretPos = 0,
+    sel, range;
+  if (window.getSelection) {
+    sel = window.getSelection();
+    if (sel.rangeCount) {
+      range = sel.getRangeAt(0);
+      if (range.commonAncestorContainer.parentNode == editableDiv) {
+        caretPos = range.endOffset;
+      }
+    }
+  } else if (document.selection && document.selection.createRange) {
+    range = document.selection.createRange();
+    if (range.parentElement() == editableDiv) {
+      var tempEl = document.createElement("span");
+      editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+      var tempRange = range.duplicate();
+      tempRange.moveToElementText(tempEl);
+      tempRange.setEndPoint("EndToEnd", range);
+      caretPos = tempRange.text.length;
+    }
+  }
+  return caretPos;
+}
+// end of credit
+
 // color the keywords
-function colorKeywords() {
+function colorKeywords(key) {
   let text = document.getElementById('text');
   // wrap the keywords in spans
+  let cursorStart = getCaretPosition(text);
   for (let i = 0; i < keywords.length; i++) {
 
-    // get text before and after selection
-    let selectionStart = text.selectionStart;
-    let selectionEnd = text.selectionEnd;
-    let before = text.value.substring(0, selectionStart);
-    let after = text.value.substring(selectionEnd, text.value.length);
-   
-    // wrap every word in a span
-    for (let word in keywords[i]) {
-      let regex = new RegExp(word, 'g');
-      text.innerHTML = text.innerHTML.replace(regex, '<font color="' + keywords[i][word] + '">' + word + '</font>');
+    // get text cursor position
+    console.log(cursorStart);
 
-      // restore selection with range
-      let range = document.createRange();
-      range.setStart(text.firstChild, selectionStart);
-      range.setEnd(text.firstChild, selectionEnd);
-      let selection = window.getSelection();
-      selection.removeAllRanges();
-      selection.addRange(range);
+    // check if the keyword is in the text
+    let textContent = text.innerHTML;
+    let keyword = Object.keys(keywords[i])[0];
+    let keywordIndex = textContent.indexOf(keyword);
+    if (keywordIndex > -1) {
+      // get the keyword color
+      let keywordColor = keywords[i][keyword];
+
+      // add font color to the keyword
+      let keywordStart = keywordIndex;
+      let keywordEnd = keywordIndex + keyword.length;
+      let keywordStartText = textContent.substring(0, keywordStart);
+      let keywordText = textContent.substring(keywordStart, keywordEnd);
+      let keywordEndText = textContent.substring(keywordEnd);
+      textContent = keywordStartText + '<span style="color: ' + keywordColor + '">' + keywordText + '</span>' + keywordEndText;
+
+      // fix the text cursor position
+      if (cursorStart > keywordStart) {
+        cursorStart += 27;
+      }
     }
+
+    // set the text
+    text.innerHTML = textContent;
+
+    // set the text cursor position
+    let range = document.createRange();
+    let sel = window.getSelection();
+    range.setStart(text.childNodes[0], cursorStart);
+    range.collapse(true);
+    sel.removeAllRanges();
+    sel.addRange(range);
   }
 }
 
@@ -39,9 +86,3 @@ document.getElementById('text').addEventListener('input', colorKeywords);
 
 // color the keywords on load
 colorKeywords();
-
-// scroll to bottom on load
-window.onload = function () {
-  let text = document.getElementById('text');
-  text.scrollTop = text.scrollHeight;
-}

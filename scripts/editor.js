2577e2cf30cba6191e86750d2f4f95067854e997
Deity <59446525+saint-deity@users.noreply.github.com>
2023-01-23 21:03:24 +1000

    Update editor.js

--- scripts/editor.js
+++ scripts/editor.js
@@ -14,9 +14,18 @@ let selection_row = 1;
 
 let insert_enabled = false;
 
-function Blink () {
+function Blink (n) {
   let row = document.getElementById('row' + selection_row)
   let children = row.children;
+
+  if (n) {
+    for (let i = 0; i < children.length; i++) {
+      if (children[i].id.includes('thin')) {
+        children[i].style.visibility = 'hidden';
+      }
+    }
+    return;
+  }
 
   //.. caret :: toggle
   for (let i = 0; i < children.length; i++) {
@@ -56,6 +65,12 @@ function height_fix() {
   row.style.height = height + 'px';
 }
 
+window.addEventListener('keydown', (e) => {  
+  if (e.keyCode === 32 && e.target === document.body) {  
+    e.preventDefault();  
+  }
+});
+
 document.body.addEventListener('keydown', function (key) {
   let ignored = [112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 16, 17, 18, 20, 91, 92, 93, 144, 145, 19, 36, 33, 34, 44, 27, 38, 40]
   //.. inputs :: debug
@@ -71,13 +86,77 @@ document.body.addEventListener('keydown', function (key) {
     if (node) {
       while (node) {
         //.. node :: carry :: exception; last
-        carried.push(node.innerHTML);
+        if (node.id.includes('col')) {
+          carried.push(node.innerHTML);
+        }
+
+        //.. node :: remove
         node.parentNode.removeChild(node);
-        node = node.nextSibling;
+        node = thin.nextSibling;
       }
     }
-      //.. node :: thin space
+    
+    //.. node :: new row
+    let new_row = document.createElement('div');
+    new_row.id = 'row' + (selection_row + 1);
+    new_row.className = 'row';
+    document.getElementById('text').appendChild(new_row);
+    
+    //.. node :: new thin
+    let new_thin = document.createElement('span');
+    new_thin.id = 'thin0';
+    new_thin.className = 'thin caret';
+    new_thin.innerHTML = thinSpace;
+    new_row.appendChild(new_thin);
+
+    //.. node :: append carried
+    for (let i = 0; i < carried.length; i++) {
+      //.. node :: new col
+      let new_col = document.createElement('span');
+      new_col.id = 'col' + (i + 1);
+      new_col.className = 'col';
+      new_col.innerHTML = carried[i];
+      new_row.appendChild(new_col);
+
+      //.. node :: new thin
+      let new_thin = document.createElement('span');
+      new_thin.id = 'thin' + (i + 1);
+      new_thin.className = 'thin caret';
+      new_thin.innerHTML = thinSpace;
+      new_row.appendChild(new_thin);
+    }
+
+    //.. caret :: disable
+    Blink(true);
+
+    //.. caret :: move
+    selection_row += 1;
+    selection_col = 0;
+
+    //.. redraw linecount
+    let linecount = document.getElementById('linecount');
+    linecount.innerHTML = '';
+    for (let i = 1; i <= selection_row; i++) {
+      let line = document.createElement('li');
+      line.id = 'line' + i;
       
+      //.. line :: styles :: offsets to be below the previous row
+      let offset = 0;
+      for (let j = 1; j < i; j++) {
+        let row = document.getElementById('row' + j);
+        offset += (row.offsetHeight - 26)
+        console.log(row.offsetHeight);
+      }
+
+      line.style.marginTop = offset + 'px';
+
+      line.innerHTML = i;
+      linecount.appendChild(line);
+    }
+
+    //.. caret :: toggle
+    Blink();
+    position_tracker();
   } else if (ignored.includes(key.keyCode)) {
     //.. inputs :: igored
     return;
@@ -88,18 +167,85 @@ document.body.addEventListener('keydown', function (key) {
     let thin = row.querySelector('#thin' + selection_col);
 
     //.. node :: move range
-    if (selection_col == 0) {
-      //.. node :: end range :: exception
-      return;
-    }
-
     let node = thin.previousSibling;
-    if (!node) {
-      //.. node :: end range :: exception
-      return;
+    let thins = '';
+    if (node && node.previousSibling) {
+      thins = node.previousSibling;
+    } else {
+      if (row.children.length == 1 || selection_col == 0) {
+        //.. caret :: move
+        let new_row = document.getElementById('row' + (selection_row-1));
+        selection_col = 0;
+
+        //.. row :: final id
+        let last = new_row.children[new_row.children.length-1];
+        last = last.id;
+        last = last.replace('thin', '');
+        selection_col = parseInt(last);
+
+        //.. cols :: carry
+        let carried = [];
+        if (row.children.length > 1) {
+          for (let i = 0; i < row.children.length; i++) {
+            if (row.children[i].id.includes('col')) {
+              carried.push(row.children[i]);
+            }
+          }
+
+          let max_range = 0;
+          for (let i = 0; i < new_row.children.length; i++) {
+            if (new_row.children[i].id.includes('thin')) {
+              max_range += 1;
+            }
+          }
+
+          for (let i = 0; i < carried.length; i++) {
+            //.. node :: new col
+            let new_col = document.createElement('span');
+            new_col.id = 'col' + max_range;
+            new_col.className = 'col';
+            new_col.innerHTML = carried[i].innerHTML;
+            new_row.appendChild(new_col);
+
+            //.. node :: new thin
+            let new_thin = document.createElement('span');
+            new_thin.id = 'thin' + (max_range + 1)
+            new_thin.className = 'thin caret';
+            new_thin.innerHTML = thinSpace;
+            new_row.appendChild(new_thin);
+
+            max_range += 1;
+          }
+
+          //.. insert linecount indicator
+        let linecount = document.getElementById('linecount');
+        linecount.innerHTML = '';
+        for (let i = 0; i < document.getElementById('text').children.length; i++) {
+          let line = document.createElement('li');
+          line.innerHTML = i + 1;
+          
+          let height = document.getElementById('text').children[i].offsetHeight;
+          line.style.height = (height +4) + 'vh';
+
+          linecount.appendChild(line);
+        }
+
+          //.. caret :: disable
+          Blink(true);
+        }
+
+        //.. row :: remove
+        row.parentNode.removeChild(row);
+
+        selection_row -= 1;
+        //.. caret :: toggle
+        Blink();
+        position_tracker();
+
+        return
+      }
     }
 
-    let thins = node.previousSibling;
 
     //.. node :: remove
     if (node.id == 'thin0') {
@@ -115,15 +261,6 @@ document.body.addEventListener('keydown', function (key) {
 
     //.. node :: move caret
     selection_col -= 1;
-    if (selection_col < 0) {
-      if (selection_row == 1) {
-        //.. node :: end range :: exception
-        return;
-      }
-
-      selection_row -= 1;
-      selection_col = document.getElementById('row' + selection_row).children.length - 1;
-    }
 
     //.. caret :: toggle
     Blink();
@@ -303,34 +440,41 @@ document.body.addEventListener('keydown', function (key) {
     
     //.. node :: range fix :: following nodes
     let children = row.children;
+    let t0 = [];
+    let t1 = [];
+
     for (let i = 0; i < children.length; i++) {
-      let id = children[i].id;
-      if (id.includes('thin')) {
-        id = id.replace('thin', '');
-
-        if (id >= selection_col) {
-          if (children[i].getAttribute('data-time')) {
-            if ((Date.now() - children[i].getAttribute('data-time')) > (Date.now() - time_thins)) {
-              children[i].id = 'thin' + (parseInt(id) + 1);
-            }
-          }
-        }
-      } else if (id.includes('col')) {
-        id = id.replace('col', '');
-
-        if (id >= (selection_col-1)) {
-          if (children[i].getAttribute('data-time') & children[i].innerHTML != key) {
-            if ((Date.now() - children[i].getAttribute('data-time')) > (Date.now() - time_node)) {
-              children[i].id = 'col' + (parseInt(id) + 1);
-            }
-          }
-        }
+      if (children[i].id.includes('thin')) {
+        t0.push(children[i]);
+      } else if (children[i].id.includes('t1')) {
+        t1.push(children[i]);
       }
+    }
+
+    for (let i = 0; i < t0.length; i++) {
+      //.. node :: thin :: id set
+      t0[i].id = 'thin' + i;
+    }
+
+    for (let i = 0; i < t1.length; i++) {
+      //.. node :: col :: id set
+      t1[i].id = 'col' + i;
     }
 
     //.. caret :: toggle
     Blink();
     position_tracker();
+  }
+
+  //.. handle ctrl + key
+  if (key.ctrlKey) {
+    if (key.keyCode == 90) {
+      //.. undo :: toggle
+      undo();
+    } else if (key.keyCode == 89) {
+      //.. redo :: toggle
+      redo();
+    }
   }
 });
 
